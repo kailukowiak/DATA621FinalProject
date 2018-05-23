278f6fb73e40e008aaa8218cbc4f21858a1acf1c
ilyakats <ikats@yahoo.com>
2018-05-22 23:36:01 -0400

    Update

--- ModelSelection.R
+++ ModelSelection.R
@@ -37,14 +37,15 @@ m5imp = lm(data = imputedData, formula = log(SalePrice)~OverallCond+Condition2+
              BsmtFullBath+BsmtFinType1 + BsmtExposure +BsmtQual +LandSlope +
              LandContour+log(LotArea) + LotFrontage+ LotConfig + Utilities + 
              HouseStyle + RoofStyle + MasVnrArea +ScreenPorch+House_Age_Yrs) 
-m6TD = lm(data = transformedData, formula = log(SalePrice)~OverallCond+Condition2+
+m6TD = lm(log(SalePrice)~OverallCond+Condition2+
             Condition1+Neighborhood+MSZoning +X1stFlrSF+X2ndFlrSF+LowQualFinSF+
             KitchenQual+Fireplaces+WoodDeckSF+Functional+FullBath+BsmtFullBath+
             BsmtFinType1 + BsmtExposure +BsmtQual +LandSlope +LandContour+
             log(LotArea) + LotFrontage+ LotConfig + Utilities + HouseStyle + 
             RoofStyle + MasVnrArea +ScreenPorch+House_Age_Yrs+RoofMatl_WdShngl+
             GarageQual_abv_avg +OverallQual2_x_GrLivArea+
-            OverallQual2_x_TotRmsAbvGrd_log+OverallQual2_x_GarageCars)  
+            OverallQual2_x_TotRmsAbvGrd_log+OverallQual2_x_GarageCars,
+          data = transformedData)  
 
 # Get AIC
 AIC (m1BC, m2BCstep, m3BC, m4BC, m5imp, m6TD)
@@ -79,3 +80,53 @@ results <- cbind(index, exp(pred))
 write.csv(results, "c://temp//results_tune.csv", row.names = FALSE)
 
 summary(model_fit)
+
+library(ggplot2)
+library(ggfortify)
+autoplot(m6TD2)
+
+library(car)
+vif(m6TD2)
+alias(m6TD2)
+
+transformedData2 <- transformedData[-c(826,524,1299,89),]
+m6TD2 = lm(log(SalePrice)~OverallCond+ # Condition2+
+            Condition1+
+            MSZoning +X1stFlrSF+X2ndFlrSF+LowQualFinSF+Neighborhood+
+            KitchenQual+Fireplaces+WoodDeckSF+Functional+FullBath+BsmtFullBath+
+            BsmtFinType1 + BsmtExposure +BsmtQual +
+            LandSlope +LandContour+
+            log(LotArea) + LotFrontage+ LotConfig + HouseStyle + #Utilities + 
+            RoofStyle + MasVnrArea +ScreenPorch+House_Age_Yrs + RoofMatl_WdShngl+
+            GarageQual_abv_avg +OverallQual2_x_GrLivArea+
+            OverallQual2_x_TotRmsAbvGrd_log+OverallQual2_x_GarageCars,
+          data = transformedData2)  
+summary(m6TD2)
+pred <- predict(m6TD2, newdata=transformedTest)
+results <- cbind(index, exp(pred))
+write.csv(results, "c://temp//results_m5TD2.csv", row.names = FALSE)
+
+AIC(m6TD2)
+
+table(transformedData2$OverallCond)
+table(transformedData2$Condition1)
+table(transformedData2$Condition2) # Removed
+table(transformedData2$MSZoning)
+table(transformedData2$Neighborhood)
+table(transformedData2$KitchenQual)
+table(transformedData2$Fireplaces)
+table(transformedData2$WoodDeckSF)
+table(transformedData2$FullBath)
+table(transformedData2$BsmtFullBath)
+table(transformedData2$BsmtFinType1)
+table(transformedData2$BsmtExposure)
+table(transformedData2$BsmtQual)
+table(transformedData2$LandSlope)
+table(transformedData2$LandContour)
+table(transformedData2$LotConfig)
+table(transformedData2$Utilities) # Removed
+table(transformedData2$HouseStyle)
+table(transformedData2$RoofStyle)
+table(transformedData2$RoofMatl_WdShngl)
+table(transformedData2$GarageQual_abv_avg)
+
